3745d4579793e9a0382969c45416a69ccde4d94c
VanSharine <vanessasharine@gmail.com>
2020-09-10 16:02:26 -0300

    modelo geral do uso de maven com spring em Java

---
+++ src/main/java/br/com/pessoal/diojavaspringmaven/DioJavaSpringMavenApplication.java
@@ -0,0 +1,13 @@
+package br.com.pessoal.diojavaspringmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioJavaSpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioJavaSpringMavenApplication.class, args);
+	}
+
+}
